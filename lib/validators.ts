c9aaa312b3d7935dd388bd804428ea845089fc2e
devxyn <johnlawrencehermo@gmail.com>
2025-12-26 16:25:57 +0800

    feat: fixed slug regex

--- lib/validators.ts
+++ lib/validators.ts
@@ -57,6 +57,6 @@ export function validatePasswordStrength(password: string): {
 }
 
 export const validateSlug = (slug: string): boolean => {
-  const slugRegex = /^[a-z0-9-]+$/;
+  const slugRegex = /^[a-z0-9]+(?:-[a-z0-9]+)*$/;
   return slugRegex.test(slug);
 };
